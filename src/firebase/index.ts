8ad28e24108e07f081866d765737e93e7ddff098
Mateo B Somavilla <logoali231@gmail.com>
2026-04-09 11:59:29 -0300

    Configure valid Firebase API keys via env variables

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { useState, useEffect } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIza...", // Mock config, user should fill this
-  authDomain: "project-code-ascention.firebaseapp.com",
-  projectId: "project-code-ascention",
-  storageBucket: "project-code-ascention.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
