19d69e2c7c182071f4a6da1f7870edb545b21fa5
Mateo <logoali231@gmail.com>
2026-04-14 20:52:42 -0300

    update!21

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as any
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma
 
 export async function findUser(email: string) {
   return prisma.user.findUnique({
